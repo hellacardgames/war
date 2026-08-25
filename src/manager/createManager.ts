a374bd48835a77760e0c6cfce10313755da7ac94
Richard Gieg <richgieg@users.noreply.github.com>
2026-08-25 14:52:19 -0700

    Update lib and createManager

--- src/manager/createManager.ts
+++ src/manager/createManager.ts
@@ -23,54 +23,10 @@ export const createManager = createManagerFactory({
   leaveGame,
   sendChat,
   startGame,
-  createCustomActions: (games) => ({
-    collectCards: (gameId: string, playerId: string) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = collectCards(game, playerId);
-      if (!result.success) {
-        return { success: false, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
-    playCardFaceDown: (gameId: string, playerId: string) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = playCardFaceDown(game, playerId);
-      if (!result.success) {
-        return { success: false, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
-    playCardFaceUp: (gameId: string, playerId: string) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = playCardFaceUp(game, playerId);
-      if (!result.success) {
-        return { success: false, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
-    replenishDeck: (gameId: string, playerId: string) => {
-      const game = games.get(gameId);
-      if (!game) {
-        return { success: false, error: "gameNotFound" } as const;
-      }
-      const result = replenishDeck(game, playerId);
-      if (!result.success) {
-        return { success: false, error: result.error } as const;
-      }
-      games.set(gameId, result.game);
-      return { success: true } as const;
-    },
+  addCustomActions: (wrapAction) => ({
+    collectCards: wrapAction(collectCards),
+    playCardFaceDown: wrapAction(playCardFaceDown),
+    playCardFaceUp: wrapAction(playCardFaceUp),
+    replenishDeck: wrapAction(replenishDeck),
   }),
 });
