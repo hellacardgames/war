afc3b8be614dc552d2ae4715cb8c1245312bd8a2
Richard Gieg <richgieg@users.noreply.github.com>
2026-09-09 09:56:45 -0700

    Use removePlayer from lib

--- src/game/actions/leaveGame.ts
+++ src/game/actions/leaveGame.ts
@@ -1,6 +1,5 @@
-import { emitEvent } from "@hellacardgames/lib";
+import { emitEvent, removePlayer } from "@hellacardgames/lib";
 import { EXPIRY_EXTENSION_MS, MIN_PLAYERS } from "../constants.js";
-import { removePlayerFromGame } from "../lib/removePlayerFromGame.js";
 import { transitionGameToForfeited } from "../lib/transitionGameToForfeited.js";
 import type { Game } from "../types/Game.js";
 
@@ -11,7 +10,7 @@ export function leaveGame(game: Game, playerId: string) {
   }
 
   game = emitEvent(game, { type: "playerLeft", username: player.username });
-  game = removePlayerFromGame(game, player.id);
+  game = removePlayer(game, player.id);
 
   if (game.status === "started" && game.players.length < MIN_PLAYERS) {
     game = transitionGameToForfeited(game);
